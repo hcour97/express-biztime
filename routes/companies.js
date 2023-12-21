6096c13e9cf609990a6d37245cbbfb85f8a50478
Hannah <hcour@gmail.com>
2023-12-21 14:19:24 -0800

    slugify feature

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const ExpressError = require("../expressError");
+const slugify = require("slugify");
 let router = new express.Router();
 const db = require("../db");
 
@@ -30,7 +31,8 @@ router.get('/:code', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        const { code, name, description } = req.body;
+        const { name, description } = req.body;
+        let code = slugify(name, {lower: true})
         const results = await db.query(`INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description`, [code, name, description])
         return res.status(201).json({ company: results.rows[0]})
     } catch (err) {
